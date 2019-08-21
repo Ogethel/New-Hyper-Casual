5e2c4c4c62d8de9162b1f67c9d50c0f68ddcbeae
Ogethel <10718916@my.uvu.edu>
2019-08-21 15:27:27 -0600

    New Unity Project

---
+++ Assets/FloatData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class FloatData : ScriptableObject
+{
+    public float value = 1f;
+
+    public void UpdateValue(float number)
+    {
+        value+=number;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/TriggerBehavior.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class TriggerBehavior : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+    }
+}
