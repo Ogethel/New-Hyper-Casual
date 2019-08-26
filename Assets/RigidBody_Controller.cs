8d57c1ecabbbdf944a88d1aa4cba694e9dfcdbaa
Ogethel <10718916@my.uvu.edu>
2019-08-26 14:44:19 -0600

    Soccer Scene Work
    Just trying to figure out a simple physics Scene for fun.

--- Assets/RigidBody_Controller.cs
+++ Assets/RigidBody_Controller.cs
@@ -37,7 +37,7 @@ public class RigidBody_Controller : MonoBehaviour
         {
             _body.AddForce(Vector3.up * Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
         }
-        if (Input.GetButtonDown("Dash"))
+        if (Input.GetButtonDown("Jump"))
         {
             Vector3 dashVelocity = Vector3.Scale(transform.forward, DashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * _body.drag + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * _body.drag + 1)) / -Time.deltaTime)));
             _body.AddForce(dashVelocity, ForceMode.VelocityChange);
