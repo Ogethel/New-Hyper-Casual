fdbcbe923d460fc476488a2d12d2317ba6465213
Ogethel <10718916@my.uvu.edu>
2019-08-26 14:55:28 -0600

    Little Changes

--- Assets/RigidBody_Controller.cs
+++ Assets/RigidBody_Controller.cs
@@ -37,7 +37,7 @@ public class RigidBody_Controller : MonoBehaviour
         {
             _body.AddForce(Vector3.up * Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
         }
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             Vector3 dashVelocity = Vector3.Scale(transform.forward, DashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * _body.drag + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * _body.drag + 1)) / -Time.deltaTime)));
             _body.AddForce(dashVelocity, ForceMode.VelocityChange);
